b715fab1a99ad232010a0f9b6a62c734cb5d6f21
hantarr01 <thunderbolt655@mail.ru>
2018-11-20 12:56:50 +0300

    007

---
+++ index.php
@@ -0,0 +1,41 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+ 	<link href="css/style.css" rel="stylesheet" type="text/css">
+
+ 	<title>TestPHP</title>
+
+ </head>
+
+ <body>
+
+
+<?php
+
+$a = 1;
+
+$b = 2;
+
+if ($a < $b){
+
+echo "<div class=b1>Условие ВЕРНО и я вывелся</div>";
+
+}
+
+else{
+
+ echo "<div class=b2>Условие НЕВЕРНО и я вывелся</div>";
+
+
+}
+
+?>
+
+
+
+
+ </body>

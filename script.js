db42717a492debcc5db12da5922195e2e672327f
hantarr01 <thunderbolt655@mail.ru>
2018-11-25 19:09:37 +0300

    008

---
+++ script.js
@@ -0,0 +1,18 @@
+function poisk() {
+  alert('Search is at the moment inaccessible!');
+}
+
+function leave() {
+  alert('At the moment it is impossible to leave the comment.');
+}
+
+
+  $(document).ready(function(){
+      $('.slider').bxSlider({
+            randomStart: true,
+            auto: true,
+            pause: 2800,
+            controls: false,
+        });    
+    });
+ 

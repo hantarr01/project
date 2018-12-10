b3693cba2dfdbbd44eff652544f0784e04a14d5e
hantarr01 <thunderbolt655@mail.ru>
2018-12-10 10:58:48 +0300

    011/10

--- jq.js
+++ jq.js
@@ -1,13 +1,3 @@
-function block(name){
-   this.height = 250;
-   this.width = 150;
-};
-
-function small(name){ 
-   this.height = 50;
-   this.width = 30;
-};
-
 $(Start).click(function(){
 	$(block).animate({
 		left: '5vw',
@@ -16,41 +6,28 @@ $(Start).click(function(){
 		width: '30vw',
 		opacity: 0.9,
 		fontSize: "16px",
-	}); 
-	$(photo).animate({
-		left: '3vw',
-		top:'6vh',
-		height: '30vh',
-		width: '30vw',
-		opacity: 0.9,
-	}); 
+	});  
 });
 
 $(Small).click(function(){
 	$(block).animate({
 		height: '-=5vw',
-		width: '-=47vh', 
-	}); 
+		width: '-=40vh', 
+	});
 });
-
 
 $(Big).click(function(){
 	$(block).animate({
 		height: '+=5vw',
-		width: '+=48vh', 
+		width: '+=40vh', 
 	}); 
 });
 
-
-
-
-var i = 5;
-while (i) {
-  i--;
-
-var exit = block-small;
-
-if (block<small){
+function Res(){
+if ($(Small).click(function(){
+	$(block).animate({
+	}); 	
+	}))
 		$(block).animate({
 			left: '3vw',
 			top:'6vh',
@@ -59,8 +36,8 @@ if (block<small){
 			opacity: 0.1,
 			fontSize: "1px",   
 		});         
-}
-};
+
+};	
 
 $(Back).click(function(){
 	$(block).animate({
@@ -70,15 +47,5 @@ $(Back).click(function(){
 		width: '1px',
 		opacity: 0.1,
 		fontSize: "1px",   
-	}); 
-
-	$(photo).animate({
-		left: '3vw',
-		top:'6vh',
-		height: '1px',
-		width: '1px',
-		opacity: 0.1,
-		height: '1px',
-		width: '1px',
-	}); 
+	});  
 });
